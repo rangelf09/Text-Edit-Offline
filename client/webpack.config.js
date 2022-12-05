7e168f1f7af68d903f610174bfacc782c5466781
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-05 17:10:38 -0600

    added the workbox plugin and css loaders and babel webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
@@ -18,13 +17,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
 
-    module: {
-      rules: [
-        
-      ],
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+new InjectManifest({
+  swSrc: './src-sw.js',
+  swDest: './src-sw.js',
+}),
+
+new WebpackPwaManifest({
+  name: 'Workbox',
+  short_name: 'Workbox',
+  description: 'Workbox is a service worker and manifest file generator.',
+  background_color: '#ffffff',
+  theme_color: '#663399',
+  start_url: './',
+  publicPath: './',
+  icons: [
+    {
+    src: path.resolve('src/images/logo.png'),
+    sizes: [96, 128, 192, 256, 384, 512],
+    destination: path.join('assets', 'icons'),
     },
-  };
+  ],
+})
+],
+// TODO: Add CSS loaders and babel to webpack.
+ module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", 'css-loader'],
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+    }
+  }
+},
+        
+  ],
+    },
+
+};
 };
